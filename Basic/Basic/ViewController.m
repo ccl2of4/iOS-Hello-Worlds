ce56366ea4007ba1eed5a6989c6dd43b3fb91345
ccl2of4 <ccl2of4@tx.rr.com>
2016-02-02 10:37:38 -0600

    Basic-ProgrammaticLayout

--- Basic/Basic/ViewController.m
+++ Basic/Basic/ViewController.m
@@ -15,7 +15,8 @@
 @end
 
 // This will make a label with the text "Hello, world!"
-// and center it in the screen.
+// and put it at ((x = 10, y = 10), (w = 100, y= 100)).
+// Coordinates are measured from the top left of the screen.
 @implementation ViewController
 
 - (void)viewDidLoad
@@ -29,41 +30,12 @@
     self.label = [[UILabel alloc] init];
     
     [self.label setText:@"Hello, world!"];
-    [self.label setBackgroundColor:[UIColor lightGrayColor]];
-    [self.label setTextColor:[UIColor darkGrayColor]];
+    [self.label setFrame:CGRectMake(10.0f, 10.0f, 100.0f, 100.0f)];
     
     // If this line is removed, the label will not be added to the view
     // hierarchy, and as a consequence it will be entirely ignored by
     // the view controller.
     [self.view addSubview:self.label];
-}
-
-/*
- * -viewDidLoad is called once near the beginning of the view controller's lifecycle,
- * while -viewDidLayoutSubviews is called any time a change is made to the root view's frame/bounds.
- *
- * Layout logic that depends on the frame/bounds of the root view must be done here
- * as opposed to -viewDidLoad so that logic is based on the correct numbers.
- *
- * Alternatively, this can all be simplified by using constraints, which are shown in another project.
- */
-- (void)viewDidLayoutSubviews
-{
-    [super viewDidLayoutSubviews];
-    [self layoutLabel];
-}
-
-- (void)layoutLabel
-{
-    CGRect bounds = self.view.bounds;
-    CGRect frame = CGRectZero;
-    
-    frame.size.width = bounds.size.width * .80;
-    frame.size.height = bounds.size.height * .80;
-    frame.origin.x = bounds.origin.x + (bounds.size.width * .2);
-    frame.origin.y = bounds.origin.y + (bounds.size.height * .2);
-    
-    self.label.frame = frame;
 }
 
 @end
